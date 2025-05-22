0949b3e3f08236abbb2826cb010a2afb33d4aa62
smeep-kaur <smeep-kaur@myseneca.ca>
2025-05-22 14:54:55 -0400

    smeep-kaur lan2f.py

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author:smeep-kaur
+# Author ID:smeep-kaur@myseneca.ca
+# Date Created: 2025/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
