bd1173330df8a7692f50848be34537131ced1afb
smeep-kaur <smeep-kaur@myseneca.ca>
2025-05-22 14:51:12 -0400

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author:smeep-kaur
+# Author ID:smeep-kaur@myseneca.ca
+# Date Created: 2025/05/22
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
